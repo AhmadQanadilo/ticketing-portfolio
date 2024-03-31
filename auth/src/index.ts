526069d1801244a78d04b344dfa0ee1879b7d7ad
Ahmad Qanadilo <Aqanadilo@aspirejo.local>
2024-04-01 00:03:10 +0300

    adding mongodb and removing node moudels

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -20,8 +22,18 @@ app.all("*", async () => {
   throw new NotFoundError();
 });
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log("listening on port 3000 "
-)})
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongodb-srv:27017/auth");
+    app.listen(3000, () => {
+      console.log("listening on port 3000 ");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
