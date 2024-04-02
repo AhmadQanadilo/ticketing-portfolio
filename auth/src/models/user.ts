c49f0e6943b2bd5cb6e9f428e04a41ecbf789105
Ahmad Qanadilo <Aqanadilo@aspirejo.local>
2024-04-02 20:02:42 +0300

    -saving user to database

---
+++ auth/src/models/user.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+interface UserAtter {
+  email: string;
+  password: string;
+}
+
+interface UserDocument extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDocument> {
+  build(attr: UserAtter): UserDocument;
+}
+
+const UserSchema = new mongoose.Schema({
+  emil: {
+    type: "string",
+    required: true,
+  },
+  password: {
+    type: "string",
+    required: true,
+  },
+});
+
+UserSchema.statics.build = (attr: UserAtter) => {
+  return new User(attr);
+};
+const User = mongoose.model<UserDocument, UserModel>("User", UserSchema);
+
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,32 +1,48 @@
 import express, {Request, Response, NextFunction} from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-errors";
 import { DatabaseConnectionError } from "../errors/database-connection-errors";
+import { BadRequestError } from "../errors/bad-request-error";
+
 const router = express.Router();
 
-router.post('/api/users/signup', 
-[
-    body('email')
-        .isEmail()
-        .withMessage('Please enter a valid email address'),
-    body('password')
-        .trim()
-        .isLength({min:4, max:20})
-        .withMessage('Please enter a valid password'),
-        (req:Request, res:Response, next:NextFunction) => {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                throw new RequestValidationError(errors.array())
-            }
-            // If validation passes, continue to the next middleware or route handler
-            next();
-          }
-]
-, (req:Request, res:Response) => {
-    const { email, password} = req.body;
-    console.log('creating a new user')
-    throw new DatabaseConnectionError()
-    res.send({})
-})
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Please enter a valid email address"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Please enter a valid password"),
+    (req: Request, res: Response, next: NextFunction) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+      }
+      // If validation passes, continue to the next middleware or route handler
+      next();
+    },
+  ],
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      console.log("User already exists");
+      throw new BadRequestError("User already exists");
+    }
+
+    try {
+      const user = User.build({ email, password });
+      await user.save();
+      console.log("User", user);
+      res.status(201).send(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 export  {router as signupRouter}
